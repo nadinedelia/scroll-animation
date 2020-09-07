7b0172be6ec76dec3348dac8c942b3c7bd260b8c
nadinedelia <nadine.delia@yahoo.com>
2020-09-07 22:44:53 +0200

    plane doing loops

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ gsap.registerPlugin(MotionPathPlugin);
 
 const tween = gsap.timeline();
 tween.to(".paper-plane", {
-  duration: 10,
+  duration: 5,
   ease: "power1.inOut",
   motionPath: {
-    path: [{x: 100, y: -20}, {x: 300, y: 10}, {x: 500, y: -20}, {x: 700, y: 10}], 
+    path: [{x: 100, y: -20}, 
+         {x: 300, y: 10},
+         {x: 500, y: 100}, 
+         {x: 750, y: -100}, 
+         {x: 300, y: -50}, 
+         {x: 600, y: 100}, 
+         {x: 800, y: 0},
+         {x: window.innerWidth, y: 150}
+        ], 
     curviness: 1.25,
     autoRotate: true
   }
