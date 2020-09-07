7da509f30f0fdabeddf759bdae9831291780f7cf
nadinedelia <nadine.delia@yahoo.com>
2020-09-07 22:36:33 +0200

    plane moving across the screen

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ gsap.registerPlugin(MotionPathPlugin);
 
 const tween = gsap.timeline();
 tween.to(".paper-plane", {
-  duration: 1,
+  duration: 10,
   ease: "power1.inOut",
   motionPath: {
-    path: [{x: 100, y: -20}], 
-    curviness: 2,
+    path: [{x: 100, y: -20}, {x: 300, y: 10}, {x: 500, y: -20}, {x: 700, y: 10}], 
+    curviness: 1.25,
     autoRotate: true
   }
 });
